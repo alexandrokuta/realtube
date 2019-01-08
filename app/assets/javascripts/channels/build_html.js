b1dd5eef0f039813ffa34a31dd13c0b70e59a482
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2019-01-08 16:45:46 +0900

    validate null content

--- app/assets/javascripts/channels/build_html.js
+++ app/assets/javascripts/channels/build_html.js
@@ -2,6 +2,7 @@ function buildHtml(data) {
   var html = `<div class="message__content">${data.content}</div>`
   $('.chat__contents').append(html);
   $('.chat__contents').animate({scrollTop: $('.message__content')[0].scrollHeight});
+  var newSrc =
   var newSrc = "https://www.youtube.com/embed/" + data.youtube
   $('#player').attr('src', newSrc);
 }
