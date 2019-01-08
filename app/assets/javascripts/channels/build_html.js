55a14efc911e238639fbe295e16a103fef1687cb
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2019-01-09 01:03:23 +0900

    ビュー編集

--- app/assets/javascripts/channels/build_html.js
+++ app/assets/javascripts/channels/build_html.js
@@ -2,8 +2,10 @@ function buildHtml(data) {
   if(data.content){
   var html = `<div class="message__content" data-user_id=${data.user.id}>${data.user.nickname}:${data.content}</div>`
   $('.chat__contents').append(html);
-  $('.chat__contents').animate({scrollTop: $('.message__content')[0].scrollHeight});
+  $('.chat__contents').animate({scrollTop: $('.chat__contents')[0].scrollHeight});
 }
+  if(data.youtube){
   var newSrc = "https://www.youtube.com/embed/" + data.youtube
   $('#player').attr('src', newSrc);
+}
 }
