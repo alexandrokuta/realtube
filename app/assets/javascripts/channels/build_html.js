7921562bb888254f7317a3a89d9082504b0348a8
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2019-01-08 17:05:55 +0900

    validates content かyoutubeが投稿されれば

--- app/assets/javascripts/channels/build_html.js
+++ app/assets/javascripts/channels/build_html.js
@@ -1,8 +1,9 @@
 function buildHtml(data) {
+  if(data.content){
   var html = `<div class="message__content">${data.content}</div>`
   $('.chat__contents').append(html);
   $('.chat__contents').animate({scrollTop: $('.message__content')[0].scrollHeight});
-  var newSrc =
+}
   var newSrc = "https://www.youtube.com/embed/" + data.youtube
   $('#player').attr('src', newSrc);
 }
