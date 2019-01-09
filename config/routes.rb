d1ba010900a787965d060231a0bd013b7f1ca6ac
alexandrokuta <alexandrokuta@ritsutsujitomohironoMacBook-ea.local>
2019-01-09 18:51:32 +0900

    root訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :rooms
+  resources :rooms,only:[:index,:show,:new,:create]
   mount ActionCable.server => '/cable'
   root 'rooms#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
